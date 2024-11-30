0119f8e11cff56b8284529d8cb7dc372aa99d6ae
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-11-30 11:45:35 +0500

    Tailwind CSS website Completed

--- Tailwind CSS/tailwind.config.js
+++ Tailwind CSS/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideInFromBottom: {
+          '0%': { opacity: '0', transform: 'translateY(50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        slideInFromBottom: 'slideInFromBottom 1.5s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
