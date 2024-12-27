5c894d63d40e1b2d2e900b406efa7a75043c6f09
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-12-27 22:23:43 +0500

    Completed

--- Chai aur Code/AI_Image_Genertor/index.js
+++ Chai aur Code/AI_Image_Genertor/index.js
@@ -1,14 +1,21 @@
-let API_Key = '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';
-let input_API = document.querySelector('#prompt');
-let button = document.querySelector('.generate');
+
+let API_Key ='';
+let input_API = document.querySelector('.prompt');
+let button = document.querySelector('.generate-image');
 let image = document.querySelector('img');
+let icon = document.querySelector('.icon');
+let downloadButton = document.querySelector('.download-button');
+
 
 async function query(data) {
+    image.classList.remove('hidden');
+    image.src = 'loading GIF.gif'; 
+    icon.style.display = 'none';
     const response = await fetch(
         "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
         {
             headers: {
-                Authorization: `Bearer ${API_Key}`,  // Fixed the error here
+                Authorization: `Bearer ${API_Key}`,  
                 "Content-Type": "application/json",
             },
             method: "POST",
@@ -19,10 +26,18 @@ async function query(data) {
     return result;
 }
 
-button.addEventListener('click', async function() {
-    query({"inputs": `${input_API.value}`}).then((response) => {
+button.addEventListener('click', async function () {
+    query({ "inputs": `${input_API.value}` }).then((response) => {
         const object_URL = URL.createObjectURL(response);
-        image.src = object_URL; 
-        console.log('Done')
+        icon.style.display = 'none'; // Hides the icon
+
+        // Update image source
+        image.src = object_URL;
+
+        // Show download button and set its href
+        downloadButton.classList.remove('hidden');
+        downloadButton.href = object_URL;
+
+        console.log('Image URL:', object_URL);
     });
 });
