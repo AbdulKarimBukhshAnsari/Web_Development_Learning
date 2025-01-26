94b93dbfcbaaa95b22661dd39aeca8419bf09b3b
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-01-26 22:40:51 +0500

    router completed

--- React/React Router/src/components/Header/header.jsx
+++ React/React Router/src/components/Header/header.jsx
@@ -54,6 +54,16 @@ export default function Header() {
                                     About
                                 </NavLink>
                             </li>
+                            <li>
+                            <NavLink
+                                    to="/contact"
+                                    className={({isActive}) =>
+                                        `block py-2 ${isActive ? "text-orange-700" : "text-gray-700" } pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
                             
                             
                         </ul>
--- React/React Router/src/main.jsx
+++ React/React Router/src/main.jsx
@@ -2,30 +2,50 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Layout from './Layout.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
 import Home from './components/Home/home.jsx'
 import About from './components/About/about.jsx'
+import Contact from './components/Contacts/contacts.jsx'
 
-const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Layout/>,
-        children : [
-            {
-                path : '',
-                element : <Home/>
-            }, 
-            { path : '/about',
-                element : <About />
-            }
+// const router = createBrowserRouter([
+//     {
+//         path : '/',
+//         element : <Layout/>,
+//         children : [
+//             {
+//                 path : '',
+//                 element : <Home/>
+//             }, 
+//             { path : '/about',
+//                 element : <About />
+//             },
+//             {
+//                 path : '/contact',
+//                 element : <Contact/>
+//             }
 
 
-        ]
-    }
+//         ]
+//     }
 
 
 
-])
+// ])
+
+// another way of routing 
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element = {<Layout/>}>
+            <Route path='' element = {<Home/>}/>
+            <Route path='/about' element = {<About/>}/>
+            <Route path='/contact' element = {<Contact/>}/>
+        </Route>
+    )
+)
+
+
+//Both Ways are correct 
 
 createRoot(document.getElementById('root')).render(
     <RouterProvider router={router}/>
